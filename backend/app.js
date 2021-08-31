c02bc452ce2eeeb5605b9dd25cb22831e490ef25
LepoultelS <stephane.lepoultel@outlook.com>
2021-08-31 10:26:56 +0200

    DB connect

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+// Tentative de connexion à la base de données
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
